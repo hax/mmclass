9c48aa790e0b3e7b3d32620468aa85ae01e3f765
HE Shi-Jun <hax@heshijun.net>
2015-03-11 14:12:26 +0800

    reformat

--- build.js
+++ build.js
@@ -8,11 +8,11 @@ var src = fs.readFileSync(__dirname + '/src/class.js', 'utf-8')
 
 src = src.replace("'export {Class}'", 'exports.Class = Class')
 
-var result = umd.
-	replace(/\$date\$/g, new Date().toISOString()).
-	replace(/\$package_(.+?)\$/g, function (_, prop) {
+var result = umd
+	.replace(/\$date\$/g, new Date().toISOString())
+	.replace(/\$package_(.+?)\$/g, function (_, prop) {
 		return pack[prop]
-	}).
-	replace('//$content$', src)
+	})
+	.replace('//$content$', src)
 
 fs.writeFileSync(__dirname + '/dist/class.js', result)
